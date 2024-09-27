e3ab4b159ec8faf7e81ab51279399b6df35af45c
Jeremfoodo <jeremy.stoss@foodomarket.com>
2024-09-27 09:48:44 +0200

    Create suivi.py

---
+++ pages/suivi.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from plot_data import load_and_filter_data, plot_mono_vs_multi_order
+from data_processing import load_prepared_data
+
+def main():
+    st.title("Suivi des clients")
+
+    st.subheader("Historique")
+    st.write("Voici les infos sur le taux de mono-order des clients français.")
+
+    df = load_prepared_data()
+    clients_fr = load_and_filter_data(df)
+
+    fig = plot_mono_vs_multi_order(clients_fr)
+    st.plotly_chart(fig)
+
+if __name__ == "__main__":
+    main()
