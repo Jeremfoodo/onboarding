f176381a7f5f125e108a1e131487a87f584a7677
Jeremfoodo <jeremy.stoss@foodomarket.com>
2024-09-27 09:45:08 +0200

    Create home.py

---
+++ home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def main():
+    st.title("Accueil de l'application Onboarding")
+    st.write("Bienvenue dans l'application de suivi des clients.")
+
+if __name__ == "__main__":
+    main()
