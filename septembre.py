bd107c3015559ecf0f89cf3fd56b8a59bbd4ea3e
Jeremfoodo <jeremy.stoss@foodomarket.com>
2024-09-27 11:36:38 +0200

    Update septembre.py

--- septembre.py
+++ septembre.py
@@ -11,7 +11,7 @@ def main():
     
     # Filtrer les clients ayant passé leur première commande en septembre 2024
     df['Date de commande'] = pd.to_datetime(df['Date de commande'])
-    df['Date 1ere commande'] = pd.to_datetime(df['Date 1ere commande'], errors='coerce')
+    df['date 1ere commande (Restaurant)'] = pd.to_datetime(df['date 1ere commande (Restaurant)'], errors='coerce')
     september_clients = df[(df['date 1ere commande (Restaurant)'].dt.month == 9) & 
                            (df['date 1ere commande (Restaurant)'].dt.year == 2024) & 
                            (df['Pays'] == 'FR')]
